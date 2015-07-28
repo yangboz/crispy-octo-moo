ce87d992c70f89b12cd783fb5bcaedb22a68e34e
yangboz <YoungWelle@gmail.com>
2015-07-28 20:14:26 +0800

    ADDED:Ionic with FB integration barebone source code

--- Ionic/www/js/app.js
+++ Ionic/www/js/app.js
@@ -4,9 +4,9 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+angular.module('starter', ['ionic', 'starter.controllers','ngOpenFB'])
 
-.run(function($ionicPlatform) {
+.run(function($ionicPlatform,ngFB) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
@@ -19,6 +19,8 @@ angular.module('starter', ['ionic', 'starter.controllers'])
       // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
     }
+    //
+    ngFB.init({appId: '844627638977678'});
   });
 })
 
