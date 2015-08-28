cdcb2e6f08ed579d0298c569b53656b999180adc
yangboz <YoungWelle@gmail.com>
2015-08-28 15:09:30 +0800

    ADDED:IonicWebApp with UI elements

--- Spring-boot/src/main/java/crispy_octo_moo/Constants.java
+++ Spring-boot/src/main/java/crispy_octo_moo/Constants.java
@@ -13,12 +13,9 @@ public final class Constants {
     public static final String SPRING_PROFILE_FAST = "fast";
     public static final String SPRING_PROFILE_CLOUD = "cloud";
     public static final String SYSTEM_ACCOUNT = "system";
-    //Table names
-    public static final String T_NAME_CONTACT = "contact";
-    public static final String T_NAME_CONTACT_ADDR = "contact_address";
-    public static final String T_NAME_CONTACT_EMAIL = "contact_email";
-    public static final String T_NAME_CONTACT_ORGANZ = "contact_organization";
-    public static final String T_NAME_CONTACT_PHONE = "contact_phone";
-    public static final String T_NAME_CONTACT_URL = "contact_url";
-    public static final String T_NAME_USER = "user";
+    //Facebook related
+    public static final String FB_APP_ID = "844627638977678";
+    public static final String FB_APP_SECRET = "40c4065408a4878eaff77882947ee3ef";
+    public static final String FB_APP_PERMISSIONS = "ads_management,ads_read,bookmarked,email,manage_notifications,manage_pages,publish_actions,publish_pages,read_insights,read_mailbox,read_page_mailboxes,read_stream,rsvp_event,tab_added,user_about_me,user_birthday,user_education_history,user_events,user_friends,user_games_activity,user_groups,user_hometown,user_likes,user_location,user_managed_groups,user_photos,user_posts,user_relationship_details,user_relationships,user_religion_politics,user_status,user_tagged_places,user_videos,user_website,user_work_history";
+    
 }
