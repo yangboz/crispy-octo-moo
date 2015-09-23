7817b4ade9bc933966dd085215d21fa0d47bd8ab
pwu1024 <pwu1024@gmail.com>
2015-09-23 04:33:54 -0700

    add Snap415UserPostsRepository and ElectricVehicleCredit look up table

---
+++ Spring-boot/src/main/java/crispy_octo_moo/repository/Snap415UserPostsRepository.java
@@ -0,0 +1,9 @@
+package crispy_octo_moo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import crispy_octo_moo.domain.Snap415UserPosts;
+
+public interface Snap415UserPostsRepository extends MongoRepository<Snap415UserPosts, String>{
+
+}
