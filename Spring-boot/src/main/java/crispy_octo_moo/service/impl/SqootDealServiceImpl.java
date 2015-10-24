58f768c20135e6202d84dbe880a0711968a007c6
yangboz <YoungWelle@gmail.com>
2015-10-24 15:47:17 +0800

    FIXED:Sqoot parameter construction functions to get required results

--- Spring-boot/src/main/java/crispy_octo_moo/service/impl/SqootDealServiceImpl.java
+++ Spring-boot/src/main/java/crispy_octo_moo/service/impl/SqootDealServiceImpl.java
@@ -16,6 +16,8 @@ import jdk.nashorn.internal.parser.JSONParser;
 import org.apache.coyote.Request;
 
 import org.apache.http.Header;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.util.EntityUtils;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.slf4j.Logger;
@@ -27,13 +29,17 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.BufferingClientHttpRequestFactory;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import javax.ws.rs.HttpMethod;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -41,6 +47,7 @@ import java.io.StringReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.CharBuffer;
+import java.nio.charset.Charset;
 import java.util.*;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutionException;
@@ -64,14 +71,28 @@ public class SqootDealServiceImpl implements SqootDealService {
 //        String urlStr = "https://api.sqoot.com/v2/deals?api_key=" + sqootApiKey + "&category_slugs=" + category;//Invalid API url.
         String urlStr = "http://api.sqoot.com/v2/deals?api_key=" + sqootApiKey;
         LOG.info("SqootDealsObject url:" + urlStr);
+        //
         RestTemplate restTemplate = getRestTemplate();
 //        restTemplate.
-        Map params = new HashMap();
-        params.put("category_slugs", category);
-        params.put("api_key", sqootApiKey);
-//        params.put("location",locationString);
-        params.put("radius", "10");
-        String respString = restTemplate.getForObject(urlStr, String.class, params);
+//        Map params = new HashMap();
+//        params.put("category_slugs", category);
+//        params.put("api_key", sqootApiKey);
+//        params.put("query", category);
+////        params.put("location",locationString);
+//        params.put("radius", "10");
+//        String respString = restTemplate.getForObject(urlStr, String.class, params);
+        //
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Accept", MediaType.APPLICATION_JSON_VALUE);
+        //!!!Notice: this parameters should be build!!!
+        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(urlStr)
+//                .queryParam("category_slugs", category)
+                .queryParam("api_key", sqootApiKey)
+                .queryParam("query", category);
+//                .queryParam("radius", "10");
+
+        HttpEntity<?> entity = new HttpEntity<>(headers);
+        HttpEntity<String> httpEntity = restTemplate.exchange(builder.build().encode().toUri(), org.springframework.http.HttpMethod.GET, entity, String.class);
 //        LOG.info("respString:" + respString);
 //        JSONObject jsonObject = new JSONObject(respString);
 //        LOG.info("jsonObject:" + jsonObject.toString());
@@ -92,6 +113,9 @@ public class SqootDealServiceImpl implements SqootDealService {
         //
         JsonNode node = null;
         SqootDealsObject sqootDealsObject = new SqootDealsObject();
+        //@see: http://stackoverflow.com/questions/5769717/how-can-i-get-an-http-response-body-as-a-string-in-java
+//        String respString = EntityUtils.toString(httpEntity, Charset.forName("UTF-8"));
+        String respString = httpEntity.getBody();
         try {
             node = objectMapper.readTree(respString);
 //            LOG.info("objectMapper.readTree:" + node.toString());
