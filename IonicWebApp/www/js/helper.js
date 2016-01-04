34c9ea97257dcc04a96b5e526d3183d2ae55e5bc
yangboz <zhou.yangbo@intellif.com>
2016-01-04 12:42:35 +0800

    CHANGED:update the new AWS_ip:54.201.26.68 related modifications

--- IonicWebApp/www/js/helper.js
+++ IonicWebApp/www/js/helper.js
@@ -23,7 +23,7 @@ var _environments = {
             /**
              * Add any config properties you want in here for this environment
              */
-            api_endpoint_base: 'http://54.201.26.68/:8083/api',
+            api_endpoint_base: 'http://54.201.26.68:8083/api',
             api_version: '/v1/',
             ga_track_code:'UA-70169433-1'
         }
--- iOS/CrispyOctoMoo/CrispyOctoMoo/Snap415API.m
+++ iOS/CrispyOctoMoo/CrispyOctoMoo/Snap415API.m
@@ -13,7 +13,7 @@
 //@see: http://stackoverflow.com/questions/5643514/how-to-define-an-nsstring-for-global-use
 //#define DEV @"dev_aws"
 #ifdef DEV
-#define kAPIEndpointHost @"http://ec2-54-218-63-45.us-west-2.compute.amazonaws.com:8083/api/v1/"
+#define kAPIEndpointHost @"http://54.201.26.68:8083/api/v1/"
 #else//LOCAL
 #define kAPIEndpointHost @"http://localhost:8083/api/v1/"
 #endif
