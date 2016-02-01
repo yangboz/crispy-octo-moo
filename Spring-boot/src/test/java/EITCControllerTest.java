ff610df1f4f425d39ce6cd0e9ddba22d4548d578
yangboz <zhou.yangbo@intellif.com>
2016-02-01 09:58:10 +0800

    ADDED:EITCControllerTest.java with pom.xml dependency of Mock/Drools

---
+++ Spring-boot/src/test/java/EITCControllerTest.java
@@ -0,0 +1,66 @@
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.http.ContentType;
+import com.jayway.restassured.response.ValidatableResponse;
+import crispy_octo_moo.Application;
+import crispy_octo_moo.dto.EITCCreditObject;
+import org.apache.http.HttpStatus;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.IntegrationTest;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import static com.jayway.restassured.RestAssured.given;
+
+/**
+ * Created by yangboz on 02/01/16.
+ */
+//@see: http://www.jayway.com/2014/07/04/integration-testing-a-spring-boot-application/
+@RunWith(SpringJUnit4ClassRunner.class)   // 1
+@SpringApplicationConfiguration(classes = Application.class)   // 2
+@WebAppConfiguration   // 3
+@IntegrationTest("server.port:0")   // 4
+public class EITCControllerTest {
+
+
+    @Value("${local.server.port}")   // 6
+            int port;
+
+    @Before
+    public void setUp() {
+        // 7
+
+        // 9
+        RestAssured.port = port;
+//        RestAssured.port = 8082;
+    }
+
+    private String getApiPath() {
+        String apiPath = "/api" + "/v1/eitcCredit";
+        System.out.println("apiPath:" + apiPath);
+        return apiPath;
+    }
+
+    @Test
+    public void testCreate() throws JsonProcessingException {
+        EITCCreditObject anewMikey = new EITCCreditObject("Married", 3, 1500);
+        //Object to JSON in String
+        ObjectMapper mapper = new ObjectMapper();
+        String updateJsonStr = mapper.writeValueAsString(anewMikey);
+        System.out.println("created jsonStr:" + updateJsonStr);
+        //
+        ValidatableResponse resp =
+                given().accept(ContentType.JSON).
+                        contentType(ContentType.JSON).
+                        body(updateJsonStr).
+                        when().
+                        post(getApiPath()).
+                        then().
+                        statusCode(HttpStatus.SC_OK);
+    }
+}
